44a67b8fdcb5554a072cb37f288a30bd0287b8e3
ylei <2457738122@qq.com>
2024-03-20 18:39:42 +0800

    搜索框样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,18 @@
             </ul>
         </div>
         <!-- 搜索框 -->
-        <div>
-            <input type="text">
+        <div class="search-box">
+            <input type="text" placeholder="搜索">
+            <div class="search-box-btn">
+                <svg focusable="false" viewBox="0 0 24 24">
+                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                </svg>
+            </div>
+            <div class="search-box-btn">
+                <svg focusable="false" viewBox="0 0 24 24">
+                    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                </svg>
+            </div>
         </div>
     </div>
     <div v-if="state.showUploadAvatar" class="upload-box">
@@ -188,5 +198,33 @@ onMounted(loadLoginUser);
         left: 0px;
         background: rgb(0, 0, 0, .4);
         z-index: 9998;
+    }
+    .search-box {
+        position: fixed;
+        display: flex;
+        right: 50%;
+        width: 30%;
+        height: 5vh;
+        align-items: center;
+        justify-content: center;
+        background-color: #f2f2f2;
+        border-radius: 40px;
+        padding: 10px 20px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+    .search-box input[type="text"] {
+        flex: 1;
+        border: none;
+        background-color: transparent;
+        outline: none;
+        font-size: 1rem;
+        color: #333;
+    }
+    .search-box-btn {
+        width: 5%;
+        height: 5%;
+        display: flex;
+        align-items: center;
+        justify-content: center;  
     }
 </style>
